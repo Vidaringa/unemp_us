bb9e3b30dfbd311d6ba5d25ce33c9566ae22b114
Viðar Ingason <vidar@vr.is>
2019-10-09 14:20:46 +0000

    tuning

--- ur_unemp.R
+++ ur_unemp.R
@@ -59,17 +59,17 @@ byrja <- Sys.time()
 # Loop
 loop_list <- foreach(i = seq_along(val_obs), .packages = c("forecast")) %dopar% {
   gogn_loop <- UNRATE$UNRATE[i:(i + max_obs)]
-  
+
   for(j in seq_along(obs)) {
     gogn_inner_loop <- tail(gogn_loop, obs[j])
     ts_inner <- ts(gogn_inner_loop, frequency = 12)
-    
+
     fit <- auto.arima(ts_inner,
                       stepwise = FALSE)
-    
+
     fc <- forecast(fit, h = 1)$mean
-    
-    spa[j, i] <- fc
+
+    spa[i, j] <- fc
   }
   return(spa)
 }
@@ -80,55 +80,75 @@ doParallel::stopImplicitCluster()
 
 
 
+spa_for <- matrix(NA, ncol = length(obs), nrow = length(val_obs))
 
+byrja <- Sys.time()
 
-
-
-
-
-
-# 
-# for(i in seq_along(test_obs)) {
-#   gogn_loop <- UNRATE$UNRATE[i:(i + max_obs)]
-#   
-#   for(j in seq_along(obs)) {
-#     gogn_inner_loop <- tail(gogn_loop, obs[j])
-#     ts_inner <- ts(gogn_inner_loop, frequency = 12)
-#     
-#     fit <- auto.arima(ts_inner,
-#                       stepwise = FALSE)
-#     
-#     fc <- forecast(fit, h = 1)$mean
-#     
-#     spa[j, i] <- fc
-#   }
-# }
-
-
-
-
-
-
-
-
-for(i in seq_along(obs)) {
-  for(j in seq_along(train_obs)){
+for(i in seq_along(val_obs)) {
+  gogn_loop <- UNRATE$UNRATE[i:(i + max_obs)]
     
-    ur_loop <- UNRATE[j:(j + max_obs), ]
-    
-    ur_train <- tail(ur_loop, obs[i])
-    ur_train <- tail(head(ur_loop, max_obs), obs[i])
-    
-    # ur_test_loop <- tail(UNRATE, -(j+1))
-    # ur_test <- tail(head(ur_test_loop$UNRATE, max_obs), 1)
-    
-    ur_train_ts <- ts(ur_train, frequency = 12)
-    fit <- auto.arima(ur_train_ts,
-                      stepwise = FALSE)
-    
-    accu[[j, i]] <- forecast(fit, h = 1)$mean
-    
-    
+    for(j in seq_along(obs)) {
+      gogn_inner_loop <- tail(gogn_loop, obs[j])
+      ts_inner <- ts(gogn_inner_loop, frequency = 12)
+      
+      fit <- auto.arima(ts_inner,
+                        stepwise = FALSE)
+      
+      fc <- forecast(fit, h = 1)$mean
+      
+      spa_for[i, j] <- fc
   }
-  
 }
+
+Sys.time() - byrja
+
+# 5.8 klst 
+
+
+saveRDS(spa_for, "spa_obs.rds")
+
+
+# Accuracy
+
+gogn_val <- UNRATE$UNRATE[(val_obs + 1), ] %>% as.data.frame()
+gogn_val <- gogn_val %>% rownames_to_column(var = "date")
+df_spa <- as.data.frame(spa_for) %>% as_tibble()
+colnames(df_spa) <- paste0("obs_", obs)
+
+df_spa$raun <- gogn_val$UNRATE
+df_spa$date <- gogn_val$date
+
+df_rmse <- df_spa %>% 
+  pivot_longer(cols = starts_with("obs"),
+               values_to = "gildi",
+               names_to = "obs") %>% 
+  group_by(obs) %>% 
+  summarise(rmse = RMSE(gildi, raun))
+
+
+# out of sample
+
+ur_train <- head(UNRATE, -18) %>%
+  as.data.frame() %>% 
+  rownames_to_column(var = "date") %>% 
+  as_tibble()
+
+ur_test <- tail(UNRATE, 18) %>% 
+  as.data.frame() %>% 
+  rownames_to_column(var = "date") %>% 
+  as_tibble()
+
+ur_train_ts <- ts(ur_train$UNRATE, frequency = 12)
+
+fit_500 <- auto.arima(ur_train_ts,
+                      stepwise = FALSE)
+
+fc_500 <- forecast(fit_500,
+                   h = 18)
+
+fc_500 %>% 
+  autoplot(include = 20) %>% 
+  autolayer(UNRATE, series = ur_test)
+
+plot(fc_500$mean, type = "l")
+lines(ur_test$UNRATE, col = "red")
